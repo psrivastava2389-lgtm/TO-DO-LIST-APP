1a743117d7f674202869d43f865070b391f1d7ed
Palak <psrivastava2389@gmail.com>
2026-06-26 10:46:47 +0530

    created sperate func to see sno and id of tasks

--- basic_logic.py
+++ basic_logic.py
@@ -4,7 +4,18 @@ curr= mydb.cursor()
 
 
 
-def show_tasks(tasks):
+def show_tasks():
+
+    curr.execute("select * from tasks")
+    tasks = curr.fetchall()
+    if not tasks:
+        print("No tasks found.\n")
+    else:
+        print("s.no\tTask\tStatus")
+        for i,task in enumerate(tasks,start=1):
+            print(i, task[1], "Completed" if task[2] else "Not Completed")
+def show_tasks_id():
+
     curr.execute("select * from tasks")
     tasks = curr.fetchall()
     if not tasks:
@@ -12,23 +23,25 @@ def show_tasks(tasks):
     else:
         print("s.no\tTask\tStatus")
         for i in tasks:
-            print(i[0], i[1], "Completed" if i[2] else "Not Completed")
-            
+            print(i[0],i[1], "Completed" if task[2] else "Not Completed")
+
 
 
 def add_task(tasks):
     task = input("Enter new task: ")
+    tasks.append(task)
+    
     
     print("Task added!\n")
-    curr.execute("insert into tasks values (%s, %s, %s)", (tasks.index(task)+1, task, 0))
+    curr.execute("insert into tasks (task_name,status) values (%s, %s)", (task, 0))
     mydb.commit()
 
 
 def delete_task(tasks):
-    show_tasks(tasks)
+    show_tasks_id()
     if tasks:
         try:
-            num = int(input("Enter task number to delete: "))
+            num = int(input("Enter task id to delete: "))
             if 1 <= num <= len(tasks):
                 removed = tasks.pop(num - 1)
                 print(f"Deleted task: {removed}\n")
@@ -36,13 +49,15 @@ def delete_task(tasks):
                 print("Invalid task number.\n")
         except ValueError:
             print("Please enter a valid number.\n")
+    else:
+        print("No tasks to delete.\n")
+
+
 
 
 def main():
     tasks = []
     curr.execute("use to_do_list")
-    curr.execute("alter table tasks change task_checklist status int(1) default 0")
-    mydb.commit()
     
 
 
@@ -55,11 +70,12 @@ def main():
         print("2. Add Task")
         print("3. Delete Task")
         print("4. Exit")
+        
 
         choice = input("Enter your choice: ")
 
         if choice == "1":
-            show_tasks(tasks)
+            show_tasks()
         elif choice == "2":
             add_task(tasks)
         elif choice == "3":
@@ -67,6 +83,8 @@ def main():
         elif choice == "4":
             print("Goodbye!")
             break
+        
+
         else:
             print("Invalid choice. Try again.\n")
 
