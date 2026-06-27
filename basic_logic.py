950e2587a5cd6067a978604669944a565fe4bab8
Palak <psrivastava2389@gmail.com>
2026-06-27 10:22:03 +0530

    added a func- edit statud of task

--- basic_logic.py
+++ basic_logic.py
@@ -1,13 +1,17 @@
 import mysql.connector
 mydb = mysql.connector.connect(host="localhost", user="root", password="123456")
 curr= mydb.cursor()
+def fetch_data():
+    curr.execute("select * from tasks")
+    tasks = curr.fetchall()
+    return tasks
+
 
 
 
 def show_tasks():
 
-    curr.execute("select * from tasks")
-    tasks = curr.fetchall()
+    tasks = fetch_data()
     if not tasks:
         print("No tasks found.\n")
     else:
@@ -30,8 +34,7 @@ def add_task():
 
 def delete_task():
     show_tasks()
-    curr.execute("select * from tasks")
-    tasks = curr.fetchall()
+    tasks = fetch_data()
     
             
     if tasks:
@@ -51,6 +54,27 @@ def delete_task():
     else:
         print("No tasks to delete.\n")
 
+def edit_task_status():
+    show_tasks()
+    tasks = fetch_data()
+    if tasks:
+        try:
+            num=int(input("Enter task serial number to edit status: "))
+            status_input = int(input("Enter new status (1 for Completed, 0 for Not Completed): "))
+            if status_input not in (0, 1):
+                print("Invalid status. Please enter 1 or 0.\n")
+                return
+            for i,task in enumerate(tasks, start=1):
+                if i==num:
+                    curr.execute("update tasks set status=%s where task_name=%s", (status_input, task[0]))
+                    mydb.commit()
+                    print(f"updated status of task :{task[0]} is {"completed" if {task[1]} else "not completed"}")
+            if num<1 or num>i:
+                print("enter valid serial number")
+        except ValueError:
+            print("Please enter a valid number.\n")
+
+
 
 
 
@@ -68,7 +92,8 @@ def main():
         print("1. View Tasks")
         print("2. Add Task")
         print("3. Delete Task")
-        print("4. Exit")
+        print("4. Edit status of Task")
+        print("5. Exit")
         
 
         choice = input("Enter your choice: ")
@@ -80,6 +105,9 @@ def main():
         elif choice == "3":
             delete_task()
         elif choice == "4":
+            edit_task_status()
+
+        elif choice == "5":
             print("Goodbye!")
             break
         
