ee10a245cf2a457006bd916857d3db88cc62b44e
Palak <psrivastava2389@gmail.com>
2026-07-13 20:40:30 +0530

    deleted all the waste functions from backend file

--- basic_logic.py
+++ basic_logic.py
@@ -9,15 +9,6 @@ def fetch_data():
 
 
 
-def show_tasks():
-
-    tasks = fetch_data()
-    if not tasks:
-        print("No tasks found.\n")
-    else:
-        print("s.no\tTask\tStatus\tImportant")
-        for i,task in enumerate(tasks, start=1):
-            print(i, task[0], "Completed" if task[1] else "Not Completed", "important" if task[2] else "not important")
 
 
 
@@ -41,68 +32,8 @@ def delete_task_by_id(task_id):
 
 
 
-def delete_task():
-    show_tasks()
-    tasks = fetch_data()
-    
-            
-    if tasks:
-        try:
-            num = int(input("Enter task serial number to delete: "))
-            for i,task in enumerate(tasks, start=1):
-                if i==num:
-                    removed = task[0]
-                    curr.execute("delete from tasks where task_name=%s", (task[0],))
-                    mydb.commit()
-                    print(f"Deleted task: {removed}\n")
-                
-            if num<1 or num>i:
-                print("Invalid task number.\n")
-        except ValueError:
-            print("Please enter a valid number.\n")
-    else:
-        print("No tasks to delete.\n")
-
-def edit_task_status():
-    show_tasks()
-    tasks = fetch_data()
-    if tasks:
-        try:
-            num=int(input("Enter task serial number to edit status: "))
-            status_input = int(input("Enter new status (1 for Completed, 0 for Not Completed): "))
-            if status_input not in (0, 1):
-                print("Invalid status. Please enter 1 or 0.\n")
-                return
-            for i,task in enumerate(tasks, start=1):
-                if i==num:
-                    curr.execute("update tasks set status=%s where task_name=%s", (status_input, task[0]))
-                    mydb.commit()
-                    print(f"updated status of task :{task[0]} is {"completed" if {status_input} else "not completed"}")
-            if num<1 or num>i:
-                print("enter valid serial number")
-        except ValueError:
-            print("Please enter a valid number.\n")
-    else: print("no tasks added yet")
 
 
-
-def mark_important():
-    show_tasks()
-    tasks = fetch_data()
-    if tasks:
-        try:
-            num=int(input("Enter task serial number to mark important "))
-           
-            for i,task in enumerate(tasks, start=1):
-                if i==num:
-                    curr.execute("update tasks set important=%s where task_name=%s", (1, task[0]))
-                    mydb.commit()
-            if num<1 or num>i:
-                print("enter valid serial number")
-        except ValueError:
-            print("enter valid number")
-    else:
-        print("no tasks added yet")
 
 
 def toggle_task_status(id):
@@ -127,40 +58,3 @@ def toggle_task_status(id):
 
 
 
-def main():
-    
-    while True:
-        print("==== TO-DO LIST ====")
-        print("1. View Tasks")
-        print("2. Add Task")
-        print("3. Delete Task")
-        print("4. Edit status of Task")
-        print("5. Mark task important")
-        print("6. Exit")
-        
-
-        choice = input("Enter your choice: ")
-
-        if choice == "1":
-            show_tasks()
-        elif choice == "2":
-            add_task_db()
-        elif choice == "3":
-            delete_task()
-        elif choice == "4":
-            edit_task_status()
-        elif choice == "5":
-            mark_important()
-            
-
-        elif choice == "6":
-            print("Goodbye!")
-            break
-        
-
-        else:
-            print("Invalid choice. Try again.\n")
-
-
-#if __name__ == "__main__":
- #   main()
